b21cf273b9b3f7947eb2742fa42cbb326f55479c
Siddharth Singh <115733028+Siddharth2k14@users.noreply.github.com>
2026-02-19 23:16:51 +0530

    Update NavBar.jsx

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
 
                 <Typography className="text-2xl font-semibold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                     <a href="#home" className="cursor-pointer hover:text-black transition">
-                        WebCraft Agency
+                        WebShop Agency
                     </a>
                 </Typography>
             </Box>
